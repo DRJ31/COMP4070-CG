5595dc12be16799ce4f102a5af269912c6af4cdf
dengrenjie <dengrenjie31@gmail.com>
2019-03-03 12:52:16 +0800

    Add support for Apple

--- Lab1/drawings.h
+++ Lab1/drawings.h
@@ -1,7 +1,13 @@
 #ifndef CG_DRAWINGS_H
 #define CG_DRAWINGS_H
-#include <cmath>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
 #include <GL/glut.h>
+#endif
+
+#include <cmath>
 #include <vector>
 
 void draw();
