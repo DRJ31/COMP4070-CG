244bd68b59891f8b082ad997ff6cb5551ed3e8fa
dengrenjie <dengrenjie31@gmail.com>
2019-03-06 19:43:33 +0800

    Change structure

---
+++ Lab2/src/drawings.h
@@ -0,0 +1,17 @@
+#ifndef CG_DRAWINGS_H
+#define CG_DRAWINGS_H
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <cmath>
+#include <vector>
+
+void draw();
+
+void create_window();
+
+#endif //CG_DRAWINGS_H
