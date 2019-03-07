ce2f3d14cc6df7d7298174992eb461b06ad3b43b
dengrenjie <dengrenjie31@gmail.com>
2019-03-07 21:16:41 +0800

    Update snowflake

--- Lab2/src/koch_snowfake.cpp
+++ Lab2/src/koch_snowfake.cpp
@@ -1,4 +1,5 @@
 #include "drawings.h"
+#include <iostream>
 
 using namespace std;
 
@@ -41,11 +42,25 @@ void koch_snowfake(float x, float y, double angle, float length, int n) {
 }
 
 
+vector<vector<float>> get_points(float x, float y, float length, float angle) {
+    vector<vector<float>> points(3);
+    points[0].push_back(x);
+    points[0].push_back(y);
+    points[1].push_back(x + length * cos(angle));
+    points[1].push_back(y + length * sin(angle));
+    points[2].push_back(x + length * cos(angle + M_PI / 3));
+    points[2].push_back(y + length * sin(angle + M_PI / 3));
+    return points;
+}
+
+
 void draw() {
     int iters = 5;
     float half_length = 0.8;
+    float angle = -M_PI / 6;
+    vector<vector<float>> points = get_points(-half_length * sqrt(3) / 3 * 2, 0.0, half_length * 2, angle);
 
-    koch_snowfake(-half_length, half_length * (-sqrt(3)) / 3, 0.0, half_length * 2, iters);
-    koch_snowfake(half_length, half_length * (-sqrt(3)) / 3, M_PI * 2 / 3, half_length * 2, iters);
-    koch_snowfake(0, half_length * sqrt(3) / 3 * 2, -M_PI * 2 / 3, half_length * 2, iters);
+    koch_snowfake(points[0][0], points[0][1], angle, half_length * 2, iters);
+    koch_snowfake(points[1][0], points[1][1], angle + M_PI * 2 / 3, half_length * 2, iters);
+    koch_snowfake(points[2][0], points[2][1], angle -M_PI * 2 / 3, half_length * 2, iters);
 }
