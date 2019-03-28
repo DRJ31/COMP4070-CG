3f6eb0f566a39fbc2750d8c8ed77fb852437e6f1
dengrenjie <dengrenjie31@gmail.com>
2019-03-28 10:53:36 +0800

    Lab4 done

---
+++ Lab4/src/drawings.h
@@ -0,0 +1,21 @@
+#ifndef CG_DRAWINGS_H
+#define CG_DRAWINGS_H
+#define _USE_MATH_DEFINES
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <cmath>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+void sphere(double radius, const GLfloat *color);
+
+void cylinder(double radius, double height, const GLfloat *color);
+
+#endif //CG_DRAWINGS_H
